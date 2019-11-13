6ce84ef9f31fcdad2d87c180697b33049bfa433f
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2019-11-13 10:55:29 +0000

    Rover moves well around grid. Also wraps when reaches grid limits.

--- src/Rover.java
+++ src/Rover.java
@@ -6,7 +6,7 @@ public class Rover {
 
   public Rover(Pluto pluto) {
     this.pluto = pluto;
-    this.currentLocation = new Coordinate(0,0);
+    this.currentLocation = new Coordinate(0, 0);
     this.bearing = Bearing.NORTH;
   }
 
@@ -17,5 +17,49 @@ public class Rover {
   public Bearing getBearing() {
     return this.bearing;
   }
-}
 
+  public void executeCommand(String command) {
+    for (char c : command.toCharArray()) {
+      switch (c) {
+        case 'F':
+          move(1);
+          break;
+        case 'B':
+          move(-1);
+          break;
+        case 'L':
+          rotate90(-1);
+          break;
+        case 'R':
+          rotate90(1);
+          break;
+      }
+    }
+  }
+
+  private void move(int steps) {
+    int plutoHeight = pluto.getHeight();
+    int plutoWidth = pluto.getWidth();
+    switch(this.bearing) {
+      case NORTH:
+        currentLocation.y = (currentLocation.y + steps) % plutoHeight;
+        break;
+      case EAST:
+        currentLocation.x = (currentLocation.x + steps) % plutoWidth;
+        break;
+      case SOUTH:
+        currentLocation.y = (currentLocation.y - steps) % plutoHeight;
+        break;
+      case WEST:
+        currentLocation.x = (currentLocation.x - steps) % plutoWidth;
+        break;
+    }
+  }
+
+  private void rotate90(int direction) {
+    Bearing[] values = Bearing.values();
+    int bearingIndex = this.bearing.ordinal();
+    int newIndex = (bearingIndex + direction + values.length) % values.length;
+    this.bearing = values[newIndex];
+  }
+}
--- test/RoverTest.java
+++ test/RoverTest.java
@@ -5,19 +5,68 @@ import org.junit.Test;
 
 public class RoverTest {
 
+  private Pluto pluto = new Pluto(5, 5);
+  private Rover rover = new Rover(pluto);
+
   @Test
   public void plutoHasHeightAndWidthInitialisedTo() {
-    Pluto pluto = new Pluto(2,3);
-    assertEquals(pluto.getHeight(), 2);
-    assertEquals(pluto.getWidth(), 3);
+    Pluto pluto2 = new Pluto(2, 3);
+    assertEquals(pluto2.getHeight(), 2);
+    assertEquals(pluto2.getWidth(), 3);
   }
+
   @Test
   public void roverIsInitiallyFacingNorthAtOrigin() {
-    Pluto pluto = new Pluto(5, 5);
-    Rover rover = new Rover(pluto);
-
     assertEquals(rover.getPosition(), new Coordinate(0, 0));
     assertEquals(rover.getBearing(), Bearing.NORTH);
+  }
+
+  @Test
+  public void roverRotatesWellWithCommand() {
+    rover.executeCommand("R");
+    assertEquals(rover.getBearing(), Bearing.EAST);
+    rover.executeCommand("R");
+    assertEquals(rover.getBearing(), Bearing.SOUTH);
+    rover.executeCommand("R");
+    assertEquals(rover.getBearing(), Bearing.WEST);
+    rover.executeCommand("R");
+    assertEquals(rover.getBearing(), Bearing.NORTH);
+    rover.executeCommand("L");
+    assertEquals(rover.getBearing(), Bearing.WEST);
+    rover.executeCommand("L");
+    assertEquals(rover.getBearing(), Bearing.SOUTH);
+    rover.executeCommand("L");
+    assertEquals(rover.getBearing(), Bearing.EAST);
+    rover.executeCommand("L");
+    assertEquals(rover.getBearing(), Bearing.NORTH);
+  }
+
+  @Test
+  public void roverMovesForwardWithBearingCorrectly() {
+    rover.executeCommand("FFR");
+    assertEquals(rover.getPosition(), new Coordinate(0, 2));
+    rover.executeCommand("FFR");
+    assertEquals(rover.getPosition(), new Coordinate(2, 2));
+    rover.executeCommand("FR");
+    assertEquals(rover.getPosition(), new Coordinate(2, 1));
+    rover.executeCommand("FR");
+    assertEquals(rover.getPosition(), new Coordinate(1, 1));
+  }
+
+  @Test
+  public void roverWrapsAroundGrid() {
+    rover.executeCommand("FF");
+    assertEquals(rover.getPosition(), new Coordinate(0, 2));
+    for(int i = 0; i < pluto.getHeight(); ++i) {
+      rover.executeCommand("F");
+    }
+    assertEquals(rover.getPosition(), new Coordinate(0, 2));
+    rover.executeCommand("R");
+    for(int j = 0; j < pluto.getWidth(); ++j) {
+      rover.executeCommand("F");
+    }
+    assertEquals(rover.getPosition(), new Coordinate(0, 2));
+
   }
 
 
